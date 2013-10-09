2daedc565195ef9b06f322b40ca39c262400b0e3
Hannah <hyoung20@gmail.com>
2013-10-09 09:44:28 -0700

    Added Robotium jars to test project, added a robotium test to AddActivity2.
    Signed-off-by: Hannah <hyoung20@gmail.com>
    
    	new file:   robotium/robotium-solo-4.3-javadoc.jar
    	new file:   robotium/robotium-solo-4.3.jar
    	modified:   src/com/catalyst/android/birdapp/test/AddActivity2.java

--- src/com/catalyst/android/birdapp/test/AddActivity2.java
+++ src/com/catalyst/android/birdapp/test/AddActivity2.java
@@ -1,94 +1,63 @@
 package com.catalyst.android.birdapp.test;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+
 
 
 import com.catalyst.android.birdapp.AddNewActivity;
-
-import android.app.Activity;
-import android.app.Instrumentation.ActivityMonitor;
-
+import com.catalyst.android.birdapp.BirdFormActivity;
+import com.jayway.android.robotium.solo.Solo;
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.app.Activity;
+import android.widget.EditText;
+
 
 public class AddActivity2 extends ActivityInstrumentationTestCase2<AddNewActivity> {
 	private AddNewActivity addNewActivity;
+	private BirdFormActivity bfActivity;
 	private CheckBox checkBox;
 	private Button saveButton;
+	private EditText editText;
+	private static final String testString = "junitTest";
+	private Solo solo;
+	
 	public AddActivity2() {
 		super(AddNewActivity.class);
+		
 	}
-
 
 	protected void setUp() throws Exception {
 		super.setUp();
 		addNewActivity = getActivity();
+		solo = new Solo(getInstrumentation(), getActivity());
 		checkBox = (CheckBox)addNewActivity.findViewById(com.catalyst.android.birdapp.R.id.add_another_button);
 		saveButton = (Button)addNewActivity.findViewById(com.catalyst.android.birdapp.R.id.saveButton);
+		editText = (EditText)addNewActivity.findViewById(com.catalyst.android.birdapp.R.id.activityName);
 	}
 
-	
-
-	public void testCheckBoxAddActivityChecked(){
-		ActivityMonitor activityMonitor = getInstrumentation().addMonitor(AddNewActivity.class.getName(), null, false);
-		
-		
-		addNewActivity.runOnUiThread(
-			      new Runnable() {
-			        public void run() {
-			        	checkBox.performClick();
-			        	saveButton.performClick();
-			        	
-			        } 
-			      } 
-			    ); 
-		
-		Activity nextActivity = getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 10000);
-		
-		assertNotNull(nextActivity);
-		  nextActivity.finish();
-	}
-	
 	public void testCheckBoxAddActivityUnChecked(){
-		ActivityMonitor activityMonitor = getInstrumentation().addMonitor(AddNewActivity.class.getName(), null, false);
 		
-		
-		addNewActivity.runOnUiThread(
-			      new Runnable() {
-			        public void run() {
-			        	saveButton.performClick();
+	
 			        	
-			        } 
-			      } 
-			    ); 
-		
-		Activity nextActivity = getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 10000);
-		nextActivity.getTitle();
-		//assertNotNull(nextActivity);
-		assertEquals(nextActivity.getTitle(), "test");
-		  nextActivity.finish();
+			        	solo.enterText(editText, "junitTest");
+			        	solo.clickOnCheckBox(0);
+			        	solo.clickOnButton("Save");
+			        	solo.assertCurrentActivity("on AddNewActivity", AddNewActivity.class);
+			        	solo.enterText(editText, "junitTest2");
+			        	solo.clickOnButton("Save");
+			        	solo.finishOpenedActivities();
+			        	solo.assertCurrentActivity("on BirdActivityForm", BirdFormActivity.class);
+			        	
 	}
+
+
+
+	
 	
 	protected void tearDown() throws Exception {
 		super.tearDown();
 	}
-	
-//	@Test
-//
-//		 public void testSpinnerUI() {
-//
-//			 addActivity.runOnUiThread(
-//			      new Runnable() {
-//			        public void run() {
-//			        	checkBox.performClick();
-//			        	assertTrue(checkBox.isChecked());
-//			        } 
-//			      } 
-//			    ); 
-//	}
+
+
 
 }
