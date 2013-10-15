30c33aaa3667ec48165bd0f89a4639a1a09947b7
unknown <abarendt@PC20182.catalystsolves.com>
2013-10-15 15:55:11 -0700

    End of day commit, placed .git file in workspace 	modified:   AndroidManifest.xml 	new file:   proguard-project.txt 	modified:   res/values/strings.xml 	deleted:    src/com/catalyst/android/birdapp/test/AddActivity2.java 	deleted:    src/com/catalyst/android/birdapp/test/BirdAppTest.java 	new file:   src/com/catalyst/android/birdapp/test/GPSTests_Emulator_GPSOff.java 	new file:   src/com/catalyst/android/birdapp/test/GPSTests_Emulator_GPSOn.java 	new file:   src/com/catalyst/android/birdapp/test/GPSTests_Phone.java 	modified:   src/com/catalyst/android/birdapp/test/InitialFormTest.java

---
+++ src/com/catalyst/android/birdapp/test/GPSTests_Emulator_GPSOn.java
@@ -0,0 +1,91 @@
+package com.catalyst.android.birdapp.test;
+
+import android.content.Intent;
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.catalyst.android.birdapp.BirdFormActivity;
+import com.jayway.android.robotium.solo.Solo;
+
+/**
+ * @author abarendt
+ * A series of tests for the GPS/mapping functions
+ *
+ */
+public class GPSTests_Emulator_GPSOn extends
+ActivityInstrumentationTestCase2<BirdFormActivity> {
+	
+
+    private Solo solo;
+
+	public GPSTests_Emulator_GPSOn() {
+		super(BirdFormActivity.class);
+	}
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		Intent i = new Intent();
+		i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		solo = new Solo(getInstrumentation(), getActivity());
+
+	}
+
+	@Override
+	public void tearDown() throws Exception {
+		try {
+			solo.finalize();
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		getActivity().finish();
+		super.tearDown();
+	}
+
+	public void test_Zx_Autofill_GPSOff_LeaveOff() {
+	//TODo make sure the emulator is set up wit the GPS turned off 
+
+			solo.assertCurrentActivity("GPS is turned off, don't turn it on",
+					BirdFormActivity.class);
+			assertTrue(solo.waitForText("Bird Sighting Form"));
+
+			solo.clickOnCheckBox(0);
+
+			solo.sleep(2000);
+
+			solo.clickOnText("OK");
+
+			assertTrue(solo.waitForText("GPS is turned OFF"));
+			solo.clickOnButton("NO");
+			
+			assertTrue(solo.waitForText("Coordinates not available"));
+			
+			solo.finishOpenedActivities();
+		}
+
+		public void test_Zy_AutoFill_GPSOff_TurnOn() {
+
+			solo.assertCurrentActivity("GPS is turned off, turn it on",
+					BirdFormActivity.class);
+			assertTrue(solo.waitForText("Bird Sighting Form"));
+
+			solo.clickOnCheckBox(0);
+
+			solo.sleep(2000);
+
+			solo.clickOnText("OK");
+
+			assertTrue(solo.waitForText("GPS is turned OFF"));
+			solo.clickOnButton("YES");
+
+			// below GPS text and check box is on Anna's phone
+			solo.sleep(1000);
+			assertTrue(solo.searchText("Use GPS satellites"));
+			// solo.clickOnCheckBox(1);
+			// solo.goBack();
+			// below code is poor coding, need variables...
+			// assertTrue(solo.waitForText("45.51"));
+			// assertTrue(solo.waitForText("-122.83"));
+			solo.finishOpenedActivities();
+		}}
+
+
+	
--- src/com/catalyst/android/birdapp/test/InitialFormTest.java
+++ src/com/catalyst/android/birdapp/test/InitialFormTest.java
@@ -3,18 +3,20 @@ package com.catalyst.android.birdapp.test;
 import junit.framework.Assert;
 
 import com.catalyst.android.birdapp.BirdFormActivity;
-import com.google.android.gms.internal.by;
 import com.jayway.android.robotium.solo.Solo;
 
 import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
-import android.widget.TextView;
 
 /**
  * @author abarendt Robotium test cases for com.catalyst.android.birdapp
- *         BirdFormActivity Will be used for regression testing
+ *         BirdFormActivity. Will be used for regression testing
  */
 
+/**
+ * @author abarendt
+ * 
+ */
 public class InitialFormTest extends
 		ActivityInstrumentationTestCase2<BirdFormActivity> {
 
@@ -24,17 +26,13 @@ public class InitialFormTest extends
 	private String NOT_SCIENTIFIC_NAME = "NotScientificName";
 	private String NOTES = "These are the notes.";
 	private String NOT_NOTES = "These are not the notes.";
+	private String NEW_ACTIVITY = "NewActivity";
+
 	// private String SYSTEM_TIME= (String) System.currentTimeMillis();
 
 	private Solo solo;
 
 	public InitialFormTest() {
-		/*
-		 * super("com.catalyst.android.birdapp", BirdFormActivity.class); cannot
-		 * find what this depricated code has been replaced by, have found this
-		 * in several tuts even 2013 tuts.
-		 */
-
 		super(BirdFormActivity.class);
 
 	}
@@ -44,88 +42,125 @@ public class InitialFormTest extends
 		Intent i = new Intent();
 		i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 		solo = new Solo(getInstrumentation(), getActivity());
+
 	}
-  @Override
-  public void tearDown() throws Exception{
-	  try{
-		  solo.finalize();
-	  }catch (Throwable e){
-		  e.printStackTrace();
-	  }
-	  getActivity().finish();
-	  super.tearDown();
-  }
-	/*
-	 * public void testDate(){
-	 * solo.assertCurrentActivity("BirdFormActivity testDate",
-	 * BirdFormActivity.class);
-	 * Assert.assertTrue(solo.getEditText(3).getText().toString
-	 * ().equals(SYSTEM_TIME)) }
-	 * 
-	 * public void testTime(){
-	 * solo.assertCurrentActivity("BirdFormActivity testTime",
-	 * BirdFormActivity.class);
-	 * 
-	 * }
+
+	@Override
+	public void tearDown() throws Exception {
+		try {
+			solo.finalize();
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		getActivity().finish();
+		super.tearDown();
+	}
+
+	
+
+	/**
+	 * Black box test to verify that the soft keyboard appears when the common
+	 * bird text field is tapped. This may not be the best test method when
+	 * other languages are incorporated.
 	 */
-
-	public void testAutofill_GPSOff_LeaveOff() {
-		Intent i = new Intent();
-		i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		solo = new Solo(getInstrumentation(), getActivity());
-		
-		solo.assertCurrentActivity("GPS is turned off, don't turn it on",
+	public void test_KeyboardAppear_BirdName_English() {
+		solo.assertCurrentActivity("Soft Keyboard_BirdName",
 				BirdFormActivity.class);
-		assertTrue(solo.waitForText("Bird Sighting Form"));
-
-		solo.clickOnCheckBox(0);
-
-		solo.sleep(2000);
-
-		solo.clickOnText("OK");
-
-		assertTrue(solo.waitForText("GPS is turned OFF"));
-		solo.clickOnButton("NO");
-		assertTrue(solo.waitForText("Coordinates not available"));
+		solo.clickOnEditText(0);
+		solo.waitForText("q");
+		solo.searchText("z");
+		solo.goBack();
+		solo.finishOpenedActivities();
 	}
 
-	public void testAutoFill_GPSOff_TurnOn(){
-		Intent i = new Intent();
-		i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		solo = new Solo(getInstrumentation(), getActivity());
-		
-		solo.assertCurrentActivity("GPS is turned off, don't turn it on",
+	/**
+	 * Black box test to verify that the soft keyboard appears when the
+	 * scientific name text field is tapped. This may not be the best test
+	 * method when other languages are incorporated.
+	 */
+	public void test_KeyboardAppear_ScientificName_English() {
+		solo.assertCurrentActivity("Soft Keyboard_ScientificName",
 				BirdFormActivity.class);
-		assertTrue(solo.waitForText("Bird Sighting Form"));
+		solo.clickOnEditText(1);
+		solo.waitForText("q");
+		solo.searchText("z");
+		solo.goBack();
+		solo.finishOpenedActivities();
+	}
 
-		solo.clickOnCheckBox(0);
+	/**
+	 * Black box test to verify that the soft keyboard appears when the notes
+	 * text field is tapped. This may not be the best test method when other
+	 * languages are incorporated.
+	 */
+	public void test_KeyboardAppear_Notes_English() {
+		solo.assertCurrentActivity("Soft Keyboard_Notes",
+				BirdFormActivity.class);
+		solo.clickOnEditText(2);
+		solo.waitForText("q");
+		solo.searchText("z");
+		solo.goBack();
+		solo.finishOpenedActivities();
+	}
 
-		solo.sleep(2000);
+	/**
+	 * Black box test. Tests that the toast (a small pop-up box) is visible with
+	 * the correct text.
+	 */
+	public void test_Toast() {
+		solo.assertCurrentActivity("Testing the 'toast'.",
+				BirdFormActivity.class);
+		solo.enterText(0, COMMON_NAME);
+		Assert.assertTrue(solo.getEditText(0).getText().toString()
+				.equals(COMMON_NAME));
+		solo.scrollDown();
+		solo.clickOnButton("Submit");
+		assertTrue(solo.searchText("Added Bird Sighting :" + COMMON_NAME));
+		solo.finishOpenedActivities();
+	}
 
-		solo.clickOnText("OK");
+	/**
+	 * Tests that text entered into the notes field returns the entered notes
+	 * True test
+	 */
+	public void test_NotesTextField() {
 
-		assertTrue(solo.waitForText("GPS is turned OFF"));
-		solo.clickOnButton("YES");
+		solo.assertCurrentActivity("Check first activity",
+				BirdFormActivity.class);
+		solo.enterText(2, NOTES);
+		Assert.assertTrue(solo.getEditText(2).getText().toString()
+				.equals(NOTES));
+		Assert.assertFalse(solo.getEditText(2).getText().toString()
+				.equals(NOT_NOTES));
+		solo.finishOpenedActivities();
+	}
 
-	//below GPS text and check box is on Anna's phone
-		solo.sleep(1000);
-	assertTrue(solo.searchText("Use GPS satellites"));
-	//solo.clickOnCheckBox(1);
-	//solo.goBack();
-	//below code is poor coding, need variables...
-	//assertTrue(solo.waitForText("45.51"));
-	//assertTrue(solo.waitForText("-122.83"));
-}
+	/**
+	 * After the submit button has been triggered, any user-entered text will be
+	 * cleared from the Notes field.
+	 */
+	public void test_AfterSubmit_NotesTextFieldReset() {
+
+		solo.assertCurrentActivity("Check Notes Submit", BirdFormActivity.class);
+		solo.enterText(2, NOTES);
+
+		Assert.assertTrue(solo.getEditText(2).getText().toString()
+				.equals(NOTES));
+
+		solo.clickOnButton("Submit");
+
+		solo.waitForText(solo
+				.getString(com.catalyst.android.birdapp.R.string.notesHint));
+		Assert.assertTrue(solo.getEditText(2).getText().toString().equals(""));
+		solo.finishOpenedActivities();
+	}
 
 	/**
 	 * Tests that name entered into common name field returns the entered common
 	 * name True test
 	 */
-	public void testActivity_commonNameTextField() {
-		Intent i = new Intent();
-		i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		solo = new Solo(getInstrumentation(), getActivity());
-		
+	public void test_CommonNameTextField() {
+
 		solo.assertCurrentActivity("Check first activity",
 				BirdFormActivity.class);
 		solo.enterText(0, COMMON_NAME);
@@ -136,50 +171,12 @@ public class InitialFormTest extends
 	};
 
 	/**
-	 * Tests that name(s) entered into scientific name field returns the entered
-	 * scientific name True test
-	 */
-	public void testActivity_ScientificName() {
-		Intent i = new Intent();
-		i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		solo = new Solo(getInstrumentation(), getActivity());
-		
-		solo.assertCurrentActivity("Check first activity",
-				BirdFormActivity.class);
-		solo.enterText(1, SCIENTIFIC_NAME);
-		Assert.assertTrue(solo.getEditText(1).getText().toString()
-				.equals(SCIENTIFIC_NAME));
-		Assert.assertFalse(solo.getEditText(1).getText().toString()
-				.equals(NOT_SCIENTIFIC_NAME));
-	}
-
-	/**
-	 * Tests that text entered into the notes field returns the entered notes
-	 * True test
-	 */
-	public void testActivity_Notes() {
-		Intent i = new Intent();
-		i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		solo = new Solo(getInstrumentation(), getActivity());
-		
-		solo.assertCurrentActivity("Check first activity",
-				BirdFormActivity.class);
-		solo.enterText(2, NOTES);
-		Assert.assertTrue(solo.getEditText(2).getText().toString()
-				.equals(NOTES));
-		Assert.assertFalse(solo.getEditText(2).getText().toString()
-				.equals(NOT_NOTES));
-	}
-
-	/**
 	 * After the submit button has been triggered, any user-entered text will be
 	 * cleared from the Common Name field.
 	 */
-	public void testAfterSubmit_CommonNameFieldReset_True() {
-		Intent i = new Intent();
-		i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+	public void test_AfterSubmit_CommonNameFieldReset() {
 		solo = new Solo(getInstrumentation(), getActivity());
-		
+
 		solo.assertCurrentActivity("Check first activity",
 				BirdFormActivity.class);
 		solo.enterText(0, COMMON_NAME);
@@ -191,17 +188,34 @@ public class InitialFormTest extends
 				.getString(com.catalyst.android.birdapp.R.string.birdNameHint));
 
 		Assert.assertTrue(solo.getEditText(0).getText().toString().equals(""));
+		solo.finishOpenedActivities();
+	}
+
+	/**
+	 * Tests that name(s) entered into scientific name field returns the entered
+	 * scientific name True test
+	 */
+	public void test_ScientificNameTextField() {
+		solo = new Solo(getInstrumentation(), getActivity());
+
+		solo.assertCurrentActivity("Check first activity",
+				BirdFormActivity.class);
+		solo.enterText(1, SCIENTIFIC_NAME);
+		Assert.assertTrue(solo.getEditText(1).getText().toString()
+				.equals(SCIENTIFIC_NAME));
+		Assert.assertFalse(solo.getEditText(1).getText().toString()
+				.equals(NOT_SCIENTIFIC_NAME));
+		solo.finishOpenedActivities();
 	}
 
 	/**
 	 * After the submit button has been triggered, any user-entered text will be
 	 * cleared from the Scientific Name field.
 	 */
-	public void testAfterSubmit_ScientificFieldReset_True() {
-		Intent i = new Intent();
-		i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+	public void test_AfterSubmit_ScientificNameTextFieldReset() {
+
 		solo = new Solo(getInstrumentation(), getActivity());
-		
+
 		solo.assertCurrentActivity("Check Scientific Name Submit",
 				BirdFormActivity.class);
 		solo.enterText(1, SCIENTIFIC_NAME);
@@ -214,87 +228,81 @@ public class InitialFormTest extends
 		solo.waitForText(solo
 				.getString(com.catalyst.android.birdapp.R.string.scientificNameHint));
 		Assert.assertTrue(solo.getEditText(1).getText().toString().equals(""));
+		solo.finishOpenedActivities();
 	}
 
 	/**
-	 * After the submit button has been triggered, any user-entered text will be
-	 * cleared from the Notes field.
+	 * Testing the activity spinner Use Android 2.2 Activity SpinnerView has at
+	 * least 3
+	 * 
 	 */
-	public void testAfterSubmit_NotesReset_True() {
-		Intent i = new Intent();
-		i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+	// will this test work on the very first view as well as after adding a new
+	// activity?
+	public void test_ActivitySpinner() {
+
 		solo = new Solo(getInstrumentation(), getActivity());
-		
-		solo.assertCurrentActivity("Check Notes Submit", BirdFormActivity.class);
-		solo.enterText(2, NOTES);
 
-		Assert.assertTrue(solo.getEditText(2).getText().toString()
-				.equals(NOTES));
+		solo.scrollDown();
+		assertTrue(solo.searchText("Activity:"));
+		assertTrue(solo.searchText("Flying"));
+		solo.pressSpinnerItem(0, 0);
+		assertTrue(solo.searchText("Flying"));
+		solo.pressSpinnerItem(0, 1);
+		assertTrue(solo.searchText("Hunting"));
+		solo.pressSpinnerItem(0, 2);
+		assertTrue(solo.waitForText("Mating"));
+		solo.pressSpinnerItem(0, -1);
+		assertTrue(solo.waitForText("Swimming"));
 
-		solo.clickOnButton("Submit");
-
-		solo.waitForText(solo
-				.getString(com.catalyst.android.birdapp.R.string.notesHint));
-		Assert.assertTrue(solo.getEditText(2).getText().toString().equals(""));
-
+		solo.finishOpenedActivities();
 	}
 
 	/**
-	 * This test combines all of the "happy path" functions of the above tests.
+	 * Testing the Category spinner Use Android 2.2 Category SpinnerView has at
+	 * least
+	 * 
+	 * Call Solo.pressSpinneritem(1,2) Expect "Misc" to be on text line Call
+	 * Solo.pressSpinneritem(1,-1) Expect "Sighting" to be on text line
 	 */
-	// There was a problem with Eclipse "hanging up" while running the mini
-	// tests.
-	// This seems to avoid the problem. Don't know if it is based on the app
-	// submitting
-	// so many times in a row or ???
-	
-	
-	public void testBigTest() {
-		Intent i = new Intent();
-		i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		solo = new Solo(getInstrumentation(), getActivity()); 
-		
-		solo.assertCurrentActivity("Check first activity",
-				BirdFormActivity.class);
-		// common name field
-		solo.enterText(0, COMMON_NAME);
-		Assert.assertTrue(solo.getEditText(0).getText().toString()
-				.equals(COMMON_NAME));
-		Assert.assertFalse(solo.getEditText(1).getText().toString()
-				.equals(NOT_SCIENTIFIC_NAME));
+	public void test_CategorySpinner() {
+		solo = new Solo(getInstrumentation(), getActivity());
 
-		solo.enterText(1, SCIENTIFIC_NAME);
-		Assert.assertTrue(solo.getEditText(1).getText().toString()
-				.equals(SCIENTIFIC_NAME));
-		Assert.assertFalse(solo.getEditText(1).getText().toString()
-				.equals(NOT_SCIENTIFIC_NAME));
+		solo.scrollDown();
+		assertTrue(solo.searchText("Category:"));
+		assertTrue(solo.waitForText("Nest"));
+		solo.pressSpinnerItem(1, 2);
+		assertTrue(solo.waitForText("Misc"));
+		solo.pressSpinnerItem(1, -1);
+		assertTrue(solo.waitForText("Sighting"));
 
-		solo.enterText(2, NOTES);
-		Assert.assertTrue(solo.getEditText(2).getText().toString()
-				.equals(NOTES));
-		Assert.assertFalse(solo.getEditText(2).getText().toString()
-				.equals(NOT_NOTES));
-
-		solo.clickOnButton("Submit");
-
-		solo.waitForText(solo
-				.getString(com.catalyst.android.birdapp.R.string.birdNameHint));
-
-		Assert.assertTrue(solo.getEditText(0).getText().toString().equals(""));
-
-		// Scientific name field
-
-		solo.waitForText(solo
-				.getString(com.catalyst.android.birdapp.R.string.scientificNameHint));
-
-		Assert.assertTrue(solo.getEditText(1).getText().toString().equals(""));
-
-		// notes field
-
-		solo.waitForText(solo
-				.getString(com.catalyst.android.birdapp.R.string.notesHint));
-		Assert.assertTrue(solo.getEditText(2).getText().toString().equals(""));
-
+		solo.finishOpenedActivities();
 	}
 
+	/**
+	 * Testing the activity spinner Use Android 2.2 Adding activity to the
+	 * activity spinner
+	 * 
+	 * On further research: This spans two applications
+	 */
+	public void test_AddActivity() {
+		solo = new Solo(getInstrumentation(), getActivity());
+
+		// to access the phone's menu and click on "
+		solo.sendKey(Solo.MENU);
+		solo.clickOnText("Add Activity");
+
+		// to write to the "Add an Activity" form
+		assertTrue(solo.waitForText("Add an Activity"));
+		solo.enterText(0, NEW_ACTIVITY);
+
+		// save the text
+		solo.clickOnButton("Save");
+		solo.waitForText("Bird Sighting Form");
+		solo.scrollDown();
+		assertTrue(solo.searchText("Activity:"));
+		solo.pressSpinnerItem(0, 1);
+		solo.searchText(NEW_ACTIVITY);
+
+		solo.finishOpenedActivities();
+	}
 }
