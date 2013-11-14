51fe6ea0102b09d0f5c3c381febdaa291b7c11e7
Anna <ab.barendt@gmail.com>
2013-11-14 15:25:04 -0800

    changes to submit.java

--- src/com/catalyst/android/birdapp/test/Submit.java
+++ src/com/catalyst/android/birdapp/test/Submit.java
@@ -15,13 +15,20 @@ public class Submit extends ActivityInstrumentationTestCase2<BirdFormActivity> {
 	private String SCIENTIFIC_NAME = "Griseotyrannus aurantioatrocristatus aurantioatrocrista";
 	private String NOTES = "These are the notes.";
 
+	private EditText beforeNotes;
+	private EditText afterNotes;
+	private EditText beforeBird;
+	private EditText afterBird;
+	private EditText beforeSci;
+	private EditText afterSci;
+
 	private EditText birdName;
 	private EditText notes;
 	private EditText scientificName;
 
 	private int idSubmitButtonText;
 	private int idToastText;
-	
+
 	private String submitButtonText;
 	private String toastText;
 
@@ -39,35 +46,56 @@ public class Submit extends ActivityInstrumentationTestCase2<BirdFormActivity> {
 		notes = (EditText) solo.getView(id.notes_edit_text);
 		scientificName = (EditText) solo.getView(id.scientific_name_edit_text);
 		birdName = (EditText) solo.getView(id.common_name_edit_text);
-		
+
 		idSubmitButtonText = com.catalyst.android.birdapp.R.string.submitButtonText;
 		idToastText = string.sightingAddedBlankName;
-		
+
 		submitButtonText = getActivity().getResources().getString(
 				idSubmitButtonText);
 		toastText = getActivity().getResources().getString(idToastText);
 
-		
 	}
 
 	/**
-	 * When I click on the submit button
-	 * the camera will vibrate
-	 * and a toast will appear on the screen confirming my information has been saved.
+	 * When I click on the submit button the camera will vibrate and a toast
+	 * will appear on the screen confirming my information has been saved.
 	 * 
-	 * The vibration has been confirmed manually, when triggered programmatically.
-	 * The toast has been confirmed manually and programmatically when triggered programmatically.
+	 * The vibration has been confirmed manually, when triggered
+	 * programmatically. The toast has been confirmed manually and
+	 * programmatically when triggered programmatically.
 	 */
 	public void testSubmitVibrationAndToast() {
 		solo.assertCurrentActivity(" testSubmit", BirdFormActivity.class);
+
+		// before any strings are entered into the form
+		beforeNotes = notes;
+		beforeBird = birdName;
+		beforeSci = scientificName;
+
+		// text is entered
 
 		solo.enterText(birdName, BIRD_NAME);
 		solo.enterText(scientificName, SCIENTIFIC_NAME);
 		solo.enterText(notes, NOTES);
 
 		solo.clickOnButton(submitButtonText);
-		
+
 		solo.waitForText(toastText);
+
+		solo.sleep(2000);
+
+		afterNotes = notes;
+		afterBird = birdName;
+		afterSci = scientificName;
+
+		// verify fields are reset
+
+		assertEquals(beforeNotes.getText().toString(), afterNotes.getText()
+				.toString());
+		assertEquals(beforeBird.getText().toString(), afterBird.getText()
+				.toString());
+		assertEquals(beforeSci.getText().toString(), afterSci.getText()
+				.toString());
 
 
 		solo.finishOpenedActivities();
